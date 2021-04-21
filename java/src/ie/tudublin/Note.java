66d973818c675618c56c25739b41a8424d59f551
maciekgolx <macgol4321@gmail.com>
2021-04-21 18:41:30 +0100

    C19389881

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Note extends PApplet
+{
+    private char note;
+    private int duration;
+
+    public class toString
+    {
+
+    }
+}
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -2,7 +2,6 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
-
 import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
@@ -10,7 +9,11 @@ public class ScoreDisplay extends PApplet
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
-	
+	ArrayList<Integer> ArrList= new ArrayList<Integer>(8);
+	public void loadScore()
+	{
+
+	}
 	public void settings()
 	{
 		size(1000, 500);
@@ -19,6 +22,7 @@ public class ScoreDisplay extends PApplet
 		char c = '7'; // c holds the character 7 (55)
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
 		println(i);
+		
 	}
 
 	public void setup() 
